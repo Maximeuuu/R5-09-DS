e9b87bf1b769592fe856fc587c4ba6b8a725bbbb
Maxime <129526478+Maximeuuu@users.noreply.github.com>
2024-11-20 14:37:04 +0100

    Update Q6.java

--- Q6.java
+++ Q6.java
@@ -1,6 +1,6 @@
 public class Q6
 {
-	// méthode principale pour afficher les étoiles en forme de x à partir d'un nombre donné
+	// méthode principale pour afficher les étoiles en forme de x
 	public static void main( String[] args )
 	{
 		int nbEtoiles = Integer.parseInt(args[0]);
