091f11b3ecc1f92c2d2bc2aeced5dd583c2a81a7
Maxime <129526478+Maximeuuu@users.noreply.github.com>
2024-11-20 14:43:09 +0100

    Update Q6.java
    test workflow

--- Q6.java
+++ Q6.java
@@ -1,6 +1,6 @@
 public class Q6
 {
-	// méthode principale pour afficher les étoiles en forme de x
+	// méthode principale pour afficher les étoiles en forme de x en fonction du nombre donnée en paramètre
 	public static void main( String[] args )
 	{
 		int nbEtoiles = Integer.parseInt(args[0]);
